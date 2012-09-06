83f3638cad546f96a1a987e5fd54c0a92305769e
Eldar Gabdullin <eldargab@gmail.com>
2012-09-06 20:54:46 +0400

    Implement hooks

--- test/app.js
+++ test/app.js
@@ -113,12 +113,14 @@ describe('App', function () {
             assert.not.exist(app.get('undefined'))
         })
 
-        it('Throws if box is not evaluated', function () {
-            app.def('baz', function () {})
+        it('Returns undefined if box is not evaluated', function () {
+            app.def('baz', function (get, done) {})
+            app.def('bar', function () {})
 
-            ;(function () {
-                app.get('baz')
-            }).should.throw()
+            app.eval('baz')
+
+            assert.not.exist(app.get('baz'))
+            assert.not.exist(app.get('bar'))
         })
     })
 
@@ -194,6 +196,7 @@ describe('App', function () {
             })
 
             app.eval('hello', cb)
+
             onerror.calledWith('Hello error').should.be.true
             cb.called.should.be.false
         })
@@ -277,6 +280,201 @@ describe('App', function () {
             })
 
             app.eval('foo')
+        })
+    })
+
+    describe('Hooks', function () {
+        var calls
+
+        beforeEach(function () {
+            calls = ''
+        })
+
+        describe('before hook', function () {
+            it('Should be executed before box and it`s dependencies', function () {
+                app.def('foo', ['bar'], function (get) {
+                    calls += 'foo;'
+                }).def('bar', function () {
+                    calls += 'bar;'
+                }).before('foo', function () {
+                    calls += 'before;'
+                }).eval('foo')
+
+                calls.should.equal('before;bar;foo;')
+            })
+
+            it('Last defined should be executed first', function () {
+                app.def('foo', function () {
+                    calls += 'foo;'
+                }).before('foo', function () {
+                    calls += 'hook1;'
+                }).before('foo', function () {
+                    calls += 'hook2;'
+                }).eval('foo')
+
+                calls.should.be.equal('hook2;hook1;foo;')
+            })
+
+            it('Should support asynchrony', function () {
+                var hookDone
+
+                app.def('foo', function () {
+                    calls += 'foo;'
+                }).before('foo', function (get, done) {
+                    hookDone = done
+                }).eval('foo')
+
+                calls.should.equal('')
+                hookDone()
+                calls.should.equal('foo;')
+            })
+
+            it('Can have dependencies', function () {
+                app.def('foo/bar', function () {
+                    calls += 'foo/bar;'
+                    return 'bar'
+                }).def('foo', function () {
+                    calls += 'foo;'
+                }).before('foo', function (get) {
+                    calls += 'before-foo;'
+                    get('&/bar').should.equal('bar')
+                    this.get('&/bar').should.equal('bar')
+                }).eval('foo')
+
+                calls.should.equal('foo/bar;before-foo;foo;')
+            })
+
+            it('Should throw errors at box path', function (done) {
+                app.before('foo', function () {
+                    throw 'error'
+                }).onerror('foo', function (err) {
+                    err.should.equal('error')
+                    done()
+                }).eval('foo')
+            })
+        })
+
+        describe('after hook', function () {
+            it('Should be executed after box', function (done) {
+                app.def('foo', function () {
+                    return 'a'
+                }).after('foo', function (get, val) {
+                    val.should.equal('a')
+                    return 'b'
+                }).after('foo', function (get, val) {
+                    val.should.equal('b')
+                    return 'c'
+                }).eval('foo', function (val) {
+                    val.should.equal('c')
+                    done()
+                })
+            })
+
+            it('Should support asynchrony', function () {
+                var afterDone
+
+                app.def('foo', function () {
+                    calls += 'foo;'
+                }).after('foo', function (get, val, done) {
+                    calls += 'after;'
+                    afterDone = done
+                }).eval('foo', function () {
+                    calls += 'done;'
+                })
+
+                calls.should.equal('foo;after;')
+                afterDone(null, 10)
+                calls.should.equal('foo;after;done;')
+                app.get('foo').should.equal(10)
+            })
+
+            it('Can have dependencies', function () {
+                app.def('foo/bar', function () {
+                    calls += 'foo/bar;'
+                    return 'bar'
+                }).def('foo/baz', function () {
+                    calls += 'foo/baz;'
+                }).after('foo/baz', ['./bar'], function (get) {
+                    this.get('./bar').should.equal('bar')
+                    calls += 'after:foo/baz;'
+                }).eval('foo/baz')
+
+                calls.should.equal('foo/baz;foo/bar;after:foo/baz;')
+            })
+
+        })
+
+        it('Can be defined before box', function () {
+            app.before('foo', function () {
+                calls += 'before;'
+            }).after('foo', function () {
+                calls += 'after;'
+            }).def('foo', function () {
+                calls += 'foo;'
+            }).eval('foo')
+
+            calls.should.equal('before;foo;after;')
+        })
+
+
+        it('Can be defined before .set()', function () {
+            app.before('foo', function () {
+                calls += 'before;'
+            }).after('foo', function () {
+                calls += 'after;'
+            })
+
+            app.set('foo', 'bar')
+
+            assert.not.exist(app.get('foo'))
+
+            app.eval('foo')
+
+            calls.should.equal('before;after;')
+        })
+
+        it('Should be inherited from prototype app', function () {
+            var proto = new App
+
+            proto.before('foo', function () {
+                calls += 'proto-before;'
+            }).after('foo', function () {
+                calls += 'proto-after;'
+            })
+
+            proto.def('foo', function () {
+                calls += 'foo;'
+            })
+
+            var app = proto.run()
+
+            app.before('foo', function () {
+                calls += 'self-before;'
+            }).after('foo', function () {
+                calls += 'self-after;'
+            })
+
+            app.eval('foo')
+
+            calls.should.equal('self-before;proto-before;foo;proto-after;self-after;')
+        })
+
+        it('Should not touch prototype app', function () {
+            var proto = new App
+
+            proto.def('foo', function () {
+                calls += 'foo;'
+            })
+
+            proto.run().before('foo', function () {
+                calls += 'before;'
+            }).after('foo', function () {
+                calls += 'after;'
+            })
+
+            proto.eval('foo')
+
+            calls.should.equal('foo;')
         })
     })
 })
